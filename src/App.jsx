cc025fbc2034dc9acf15690527e660d357ed918e
Chisquare7 <chijiokechibuike.f@gmail.com>
2025-03-18 15:40:37 +0100

    feat: implement Home.jsx file for the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="bg-gray-100 min-h-screen p-6">
+        <Home />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import MusicCard from "../components/MusicCard";
+
+const Home = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    const getSongs = async () => {
+      const data = await fetchMusicSongs();
+      getSongs(data);
+    };
+
+    getSongs();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Music Player</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {songs.map((song) => (
+          <MusicCard key={song.id} song={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
